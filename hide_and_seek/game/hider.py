d1927dc67c8e3cb7b28bf7dd3bd8cd1018477aed
marcosarango <sar21005@byui.edu>
2021-10-11 21:19:08 -0400

    first changes

--- hide_and_seek/game/hider.py
+++ hide_and_seek/game/hider.py
@@ -1,3 +1,14 @@
 import random
 
 # TODO: Define the Hider class here
+class hider():
+    def __init__(self):
+        self.location = 0
+        self.distance = []
+
+    def get_hint():
+        pass
+    
+    def watch():
+        pass
+
