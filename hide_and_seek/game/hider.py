d89dac85870ec643e6a0123525aecbb1270c1101
marcosarango <sar21005@byui.edu>
2021-10-12 14:02:58 -0400

    I put comments in the class

--- hide_and_seek/game/hider.py
+++ hide_and_seek/game/hider.py
@@ -1,13 +1,38 @@
-from _typeshed import Self
+
 import random
 
 # TODO: Define the Hider class here
+
 class Hider():
+    """the resposability for this class is to hind for the seeker.
+    
+    steriotype:
+    Information Holder
+
+    Attributes:
+    location(int): the location of my hoder 1-100.
+    distance(list): the distance of my seeker.
+    """
     def __init__(self):
+        """ Class constructor. Instance atributes
+        
+        argument:
+          self(Hider): this is a instance of Hider
+
+        """
+        
         self.location = random.randint(1, 1000)
         self.distance = [0,0]
 
     def get_hint(self):
+        """
+        The hider give a hit fot the seeker
+
+        argument:
+           self(Hider): this a instance of Hider
+        
+           
+        """
         message_hider = "(-.-) call me when you are close" 
         if self.distance[-1] == 0:
             message_hider = (f"(;.;) You found me")
@@ -19,6 +44,9 @@ class Hider():
         return message_hider
     
     def watch(self, location):
+        """
+        This only watch the location and give distance
+        """
         distance = abs(self.location - location)
         self.distance.append(distance)
        
