eada00d9b44a0b2d94ecbfcf84ff3ccf35485ead
marcosarango <sar21005@byui.edu>
2021-10-12 13:36:41 -0400

    i did hider class

--- hide_and_seek/game/hider.py
+++ hide_and_seek/game/hider.py
@@ -1,14 +1,26 @@
+from _typeshed import Self
 import random
 
 # TODO: Define the Hider class here
-class hider():
+class Hider():
     def __init__(self):
-        self.location = 0
-        self.distance = []
+        self.location = random.randint(1, 1000)
+        self.distance = [0,0]
 
-    def get_hint():
-        pass
+    def get_hint(self):
+        message_hider = "(-.-) call me when you are close" 
+        if self.distance[-1] == 0:
+            message_hider = (f"(;.;) You found me")
+        elif self.distance[-1] < self.distance[-2]:
+            message_hider = (f"(^.^) Getting colder")
+        elif self.distance[-1] > self.distance[-2]:
+            message_hider = (f"(>.<) getting warner")
+        
+        return message_hider
     
-    def watch():
-        pass
+    def watch(self, location):
+        distance = abs(self.location - location)
+        self.distance.append(distance)
+       
+    
 
